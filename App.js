885f97008f580aca7e285b973589251ee74fa2ae
Smit Shah <smit77763@gmail.com>
2022-01-12 12:39:21 +0530

    rating v1.1

--- App.js
+++ App.js
@@ -10,12 +10,9 @@ import React, {useState} from 'react';
 import {Rating, AirbnbRating} from 'react-native-ratings';
 
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  
   StyleSheet,
   Text,
-  useColorScheme,
   Modal,
   Alert,
   View,
@@ -23,16 +20,25 @@ import {
 } from 'react-native';
 
 const App = () => {
-  function onFinishRating(rating){
-    console.log('Rating : '+rating);
+  function onFinishRating(rating) {
+    console.log('Rating : ' + rating);
     setRating(rating);
-    setTimeout(()=>{setModalVisible(!modalVisible);
-    },200)
+    setTimeout(() => {
+      setModalVisible(!modalVisible);
+    }, 200);
+
+    
     // setModalVisible(!modalVisible);
-    console.log('Rating 2: '+rating);
+    console.log('Rating 2: ' + rating);
   }
-  const WATER_IMAGE = require('./assets/rating_image/award2.png')
-  const [rating,setRating]=useState(0);
+
+  // function onSwipeRating(rating){
+  //   console.log('Swipe Rating : '+rating);
+  // }
+
+
+  const WATER_IMAGE = require('./assets/rating_image/star2.png');
+  const [rating, setRating] = useState(0);
   const [modalVisible, setModalVisible] = useState(true);
   return (
     <View style={styles.centeredView}>
@@ -47,34 +53,41 @@ const App = () => {
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             {/* <Text style={styles.modalText}>Rate user !!</Text> */}
-            <AirbnbRating
+            {/* <AirbnbRating
               count={5}
               starImage={WATER_IMAGE}
               ratingContainerStyle={styles.rating_container_style}
               starContainerStyle={styles.rating_star_style}
-              reviewColor='lightgreen'
-              selectedColor='yellow'
+              reviewColor="lightgreen"
+              selectedColor="yellow"
               reviewSize={30}
-              reviews={[
-                'Very Bad','Bad','Average','Good','Excellent'
-              ]}
+              reviews={['Very Bad', 'Bad', 'Average', 'Good', 'Excellent']}
               defaultRating={rating}
               onFinishRating={onFinishRating}
               size={35}
+            /> */}
+            <Rating
+              type="custom"
+              // ratingImage={WATER_IMAGE}
+              ratingCount={5}
+              imageSize={30}
+              showRating
+              fractions={1}
+              ratingColor='yellow'
+              jumpValue={0.1}
+              ratingBackgroundColor='grey'
+
+              ratingTextColor='lightgreen'
+              startingValue={rating}
+              tintColor='#121212'
+              // onSwipeRating={onSwipeRating}
+              onFinishRating={onFinishRating}
+              style={styles.ratingStyle}
             />
-            {/* <Rating
-  type='custom'
-  ratingImage={WATER_IMAGE}
-  ratingColor='green'
-  ratingBackgroundColor='#121212'
-   tintColor='yellow'
-  showRating
-  startingValue={0}
-  ratingCount={5}
-  imageSize={50}
-  // onFinishRating={this.ratingCompleted}
-  style={{ paddingVertical: 10 }}
-/> */}
+
+
+           
+           
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}>
@@ -90,37 +103,43 @@ const App = () => {
       </Pressable>
       <Text style={styles.ratingText}>Rating Of User</Text>
       <Text style={styles.ratingText}>{rating}</Text>
-
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  rating_container_style:{
-//  padding:10,
-  // borderColor:'red',
-  elevation:10,
-  // borderWidth:2
+  ratingStyle:{
+    paddingHorizontal:89,
+    elevation:10,
+    fontSize:30
+    // margin:
   },
-  rating_star_style:{
-// borderColor:'blue',
-elevation:10,
-padding:10,
-  },ratingText:{
-fontSize:40,
-color:'black'
+  rating_container_style: {
+    //  padding:10,
+    // borderColor:'red',
+    elevation: 10,
+    // borderWidth:2
+  },
+  rating_star_style: {
+    // borderColor:'blue',
+    elevation: 10,
+    padding: 10,
+  },
+  ratingText: {
+    fontSize: 40,
+    color: 'black',
   },
   centeredView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 22,
-    backgroundColor:'white'
+    backgroundColor: 'white',
   },
   modalView: {
     margin: 20,
     backgroundColor: '#121212',
-    opacity:0.9,
+    // opacity: 0.9,
     borderRadius: 20,
     padding: 35,
     // alignItems: 'center',
@@ -136,27 +155,27 @@ color:'black'
   button: {
     // borderRadius: 20,
     padding: 10,
-    elevation: 4,
+    elevation: 7,
   },
   buttonOpen: {
     backgroundColor: '#F194FF',
   },
   buttonClose: {
     // backgroundColor: '#2196',
-    elevation:6
-    ,paddingHorizontal:20
+    elevation: 6,
+    paddingHorizontal: 20,
   },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
-    fontSize:25
+    fontSize: 25,
   },
   modalText: {
     marginBottom: 15,
     textAlign: 'right',
     color: 'grey',
-    fontSize:30
+    fontSize: 30,
   },
 });
 
